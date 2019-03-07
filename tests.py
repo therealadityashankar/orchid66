7187c242405b56e4ec3917458aeeaa90e7d6e7a3
Aditya Shankar <aditniru@gmail.com>
2019-03-07 23:22:38 +0530

    added tests for color escape

--- tests.py
+++ tests.py
@@ -1,13 +1,28 @@
-from orchid66 import printc, printn
+from orchid66 import printc, printn, color_escape
 
 for i in range(0, 256, 20):
     # printc uses colored soooooo
-    printc("hello", (i, 100, 100), (100, 100, i), end=True)
+    if i%80:
+        end=False
+    else:
+        end=True
+    color = ((i, 100, 100), (100, 100, i))
+    printc("hello", color, end)
 
 red =  (255, 0, 0)
 blue = (50, 50, 255)
 green = (50, 255, 50)
 printn('this word is in *red*, and this is in *blue*', (red, blue))
 printn('*9&*9 is 81*', ((red, green),))
+
+
 printn('this is *red*', ('red',))
 printn('*9&*9 is 81*', (("red", "green"),))
+
+printn("escaping &*'s", ())
+string = "dua@$^**im**csck**"
+print(f"normal print (using the builtin print fn): {string}")
+escaped = color_escape(string)
+blue_in_white = ("blue", "white")
+printn(f"escaped printn: *{escaped}*", blue_in_white)
+
