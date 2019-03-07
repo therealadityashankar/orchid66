acb50cdfa12bbd0d921e608085ca95d51ad0a5c0
Aditya Shankar <aditniru@gmail.com>
2019-03-07 12:47:32 +0530

    added params to setup

--- setup.py
+++ setup.py
@@ -1,8 +1,22 @@
 import setuptools
 
+with open("README.md") as f:
+    README = f.read()
+
 setuptools.setup(
     name="orchid66",
-    version="1.0.1",
+    version="1.0.3",
+    description="print in color easily!",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/therealadityashankar/orchid66",
+    author="Aditya Shankar",
+    author_email="aditniru@gmail.com",
     license="MIT",
+    classifiers=[
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+    ],
     py_modules=["orchid66"]
 )
